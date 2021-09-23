7ab4c1723ce4bf69945467660127280849f576d3
Fisny <bardi.fisniku@gmail.com>
2021-09-23 10:49:36 +0200

    model + seeder

---
+++ database/migrations/2021_09_23_083601_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('author', 50);
+            $table->text('abstract');
+            $table->text('picture');
+            $table->year('publication_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
